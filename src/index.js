a7f28bda010e651eaa927dfdd2d2d16e10e82c3c
hereiam123 <bdemaio1@gmail.com>
2020-09-11 21:26:58 -0400

    Drawing game screen and setting initial game state

--- src/index.js
+++ src/index.js
@@ -5,6 +5,35 @@ const gameScreen = document.getElementById("gameScreen");
 
 let canvas, ctx;
 
+//State of the game
+const gameState = {
+  player: {
+    //Position of player
+    pos: {
+      x: 3,
+      y: 10,
+    },
+    //Velocity of player snake
+    vel: {
+      x: 1,
+      y: 0,
+    },
+    //Position of player snake segments
+    snake: [
+      { x: 1, y: 10 },
+      { x: 2, y: 10 },
+      { x: 3, y: 10 },
+    ],
+  },
+  //Position of food on game screen
+  food: {
+    x: 7,
+    y: 7,
+  },
+  //Size of game screen
+  gridsize: 20,
+};
+
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
